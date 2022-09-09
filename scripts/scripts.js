8ae2678a37985609b2cd14e6bdeb8d66859f7549
Christofer Lund <christofer.lund@gmail.com>
2022-09-09 23:47:28 +0200

    add difficulties, improve ai

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,72 @@
 const gameBoard = (() => {
     const winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     let board = new Array(9).fill('')
+    let isHardMode = false
+    let isEasyMode = false
+    let isExtremeMode = false
+
+    const setHardMode = () => {
+        isHardMode = true
+        isEasyMode = false
+        isExtremeMode = false
+        const easyButton = document.getElementById('easy-button')
+        const normalButton = document.getElementById('normal-button')
+        const hardButton = document.getElementById('hard-button')
+        const extremeButton = document.getElementById('extreme-button')
+
+        extremeButton.className = 'inactive-button'
+        easyButton.className = 'inactive-button'
+        normalButton.className = 'inactive-button'
+        hardButton.className = 'active-button'
+    }
+
+    const setEasyMode = () => {
+        isHardMode = false
+        isEasyMode = true
+        isExtremeMode = false
+
+        const easyButton = document.getElementById('easy-button')
+        const normalButton = document.getElementById('normal-button')
+        const hardButton = document.getElementById('hard-button')
+        const extremeButton = document.getElementById('extreme-button')
+
+        extremeButton.className = 'inactive-button'
+        easyButton.className = 'active-button'
+        normalButton.className = 'inactive-button'
+        hardButton.className = 'inactive-button'
+    }
+
+    const setNormalMode = () => {
+        isHardMode = false
+        isEasyMode = false
+        isExtremeMode = false
+
+        const easyButton = document.getElementById('easy-button')
+        const normalButton = document.getElementById('normal-button')
+        const hardButton = document.getElementById('hard-button')
+        const extremeButton = document.getElementById('extreme-button')
+
+        extremeButton.className = 'inactive-button'
+        easyButton.className = 'inactive-button'
+        normalButton.className = 'active-button'
+        hardButton.className = 'inactive-button'
+    }
+
+    const setExtremeMode = () => {
+        isHardMode = false
+        isEasyMode = false
+        isExtremeMode = true
+
+        const easyButton = document.getElementById('easy-button')
+        const normalButton = document.getElementById('normal-button')
+        const hardButton = document.getElementById('hard-button')
+        const extremeButton = document.getElementById('extreme-button')
+
+        extremeButton.className = 'active-button'
+        easyButton.className = 'inactive-button'
+        normalButton.className = 'inactive-button'
+        hardButton.className = 'inactive-button'
+    }
 
     const selectCell = (i, player) => {
         board[i] = player.symbol
@@ -20,15 +86,17 @@ const gameBoard = (() => {
     }
 
     const getHumanWinningCombos = () => {
-        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === humanPlayer.symbol).length > 1 && combo.filter(cell => board[cell] === '').length > 0)
+        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === humanPlayer.symbol).length > 1 && combo.filter(cell => board[cell] === '').length === 1)[0] ?? []
 
     }
-    const getRobotActiveCombos = () => {
-        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === aiPlayer.symbol).length === 1 && combo.filter(cell => board[cell] === '').length === 2)
+    const getRobotHalfCombos = () => {
+        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === aiPlayer.symbol).length === 1 && combo.filter(cell => board[cell] === '').length === 2)[0] ?? []
+    }
+    const getHumanHalfCombos = () => {
+        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === humanPlayer.symbol).length === 1 && combo.filter(cell => board[cell] === '').length === 2)[0] ?? []
     }
     const getRobotWinningCombos = () => {
-        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === aiPlayer.symbol).length > 1 && combo.filter(cell => board[cell] === '').length > 0)
-
+        return winningCombinations.filter(combo => combo.filter(cell => board[cell] === aiPlayer.symbol).length > 1 && combo.filter(cell => board[cell] === '').length === 1)[0] ?? []
     }
 
     const getCellValue = (i) => {
@@ -36,15 +104,19 @@ const gameBoard = (() => {
     }
 
     const onWin = (player) => {
-        const overlay = document.getElementById('overlay').className = 'visible'
-        const winner = document.getElementById('winner').innerText = player.name + ' Won!'
+        const overlay = document.getElementById('overlay')
+        const winner = document.getElementById('winner')
         const robotWon = document.getElementById('robot-won')
         const humanWon = document.getElementById('human-won')
-        if (!player.isHuman) robotWon.innerText = +robotWon.innerText + 1
-        if (player.isHuman) humanWon.innerText = +humanWon.innerText + 1
+        if (!player.isHuman) {
+            robotWon.innerText = +robotWon.innerText + 1
+        }
+        if (player.isHuman) {
+            humanWon.innerText = +humanWon.innerText + 1
+        }
+
         overlay.className = 'visible'
         winner.innerText = player.name + ' Won!'
-
     }
 
 
@@ -52,11 +124,18 @@ const gameBoard = (() => {
         const overlay = document.getElementById('overlay')
         const winner = document.getElementById('winner')
         overlay.className = 'visible'
-        winner.innerText = 'There\'s a tie. Nobody won'
+        winner.innerText = 'Tie. Nobody won'
+
+
     }
 
     const gameEnded = () => {
         return gameState.isWon(board, winningCombinations) || gameState.isTied()
+    }
+
+    const getBoard = () => {
+
+        return board
     }
 
     const clickEvent = (i) => {
@@ -64,7 +143,7 @@ const gameBoard = (() => {
 
         if (board[i] === '' && !gameEnded()) {
             selectCell(i, player)
-            if (gameState.isWon(board, winningCombinations)) {
+            if (gameState.isWon()) {
                 onWin(player)
             } else if (gameState.isTied()) {
                 onTie()
@@ -75,7 +154,7 @@ const gameBoard = (() => {
         init()
     }
 
-    const drawActive = () => {
+    const drawActiveCell = () => {
         document.getElementById('active-player').innerText = gameState.getPlayer().name
     }
     const drawBoard = () => {
@@ -96,13 +175,39 @@ const gameBoard = (() => {
         init()
     }
 
-    const init = () => {
-
-        drawActive()
+    const init = async () => {
+        if(isEasyMode) setEasyMode()
+        if(isHardMode) setHardMode()
+        if(isExtremeMode) setExtremeMode()
+        if(!isHardMode && !isEasyMode && !isExtremeMode) setNormalMode()
+        drawActiveCell()
         drawBoard()
         !gameEnded() && gameState.checkIfAi()
     }
-    return {getRobotActiveCombos ,getCellValue, init, reset, getFreeCells, winningCombinations, getHumanWinningCombos, getRobotWinningCombos}
+
+    const getDifficulty = () => {
+        return isEasyMode ? 0.1 : isHardMode ? 0.8 : isExtremeMode ? 1.0 : 0.5
+    }
+    const getWinningCombinations = () => {
+        return winningCombinations
+    }
+    return {
+        getRobotHalfCombos,
+        getHumanHalfCombos,
+        getCellValue,
+        init,
+        reset,
+        getFreeCells,
+        getWinningCombinations,
+        getHumanWinningCombos,
+        getRobotWinningCombos,
+        getBoard,
+        getDifficulty,
+        setEasyMode,
+        setNormalMode,
+        setHardMode,
+        setExtremeMode
+    }
 })()
 
 
@@ -112,20 +217,27 @@ const Player = (name, symbol, isHuman) => {
 }
 
 const ai = (() => {
-    const makeMove = () => setTimeout(() => {
+
+
+    const selectBestMove = () => {
+        const freeCells = gameBoard.getFreeCells()
         const robotCombos = gameBoard.getRobotWinningCombos()
-        const activeCombos = gameBoard.getRobotActiveCombos()
+        const humanHalfCombos = gameBoard.getHumanHalfCombos()
         const humanCombos = gameBoard.getHumanWinningCombos()
+        const robotHalfCombos = gameBoard.getRobotHalfCombos()
 
+        const difficultyRating = Math.random()
+        const difficulty = gameBoard.getDifficulty()
+        const moves = difficultyRating > difficulty ? freeCells : robotCombos.length ? robotCombos : humanCombos.length ? humanCombos : robotHalfCombos.length ? robotHalfCombos : freeCells.includes(4) ? [4] : humanHalfCombos.length ? humanHalfCombos : freeCells
 
-        const winGame = robotCombos.map(combo => combo.filter(cell => gameBoard.getCellValue(cell) === '')[0])
-        const sabotage = humanCombos.map(combo => combo.filter(cell => gameBoard.getCellValue(cell) === '')[0])
-        const active = activeCombos.map(combo => combo.filter(cell => gameBoard.getCellValue(cell) === '')[0])
-        const free = winGame.length ? winGame : sabotage.length ? sabotage : active.length ? active : gameBoard.getFreeCells()
-        const cellNum = free[Math.floor(Math.random() * free.length)]
-        const cell = document.getElementById(`button-${cellNum}`)
+        return moves[Math.floor(Math.random() * moves.length)]
+    }
+    const makeMove = () => setTimeout(() => {
+        const selected = selectBestMove()
+        const cell = document.getElementById(`button-${selected}`)
         cell && cell.click()
-    }, 300)
+    }, 100)
+
     return {makeMove}
 })()
 
@@ -153,7 +265,9 @@ const gameState = (() => {
         gameBoard.reset()
     }
 
-    const isWon = (board, winningCombinations) => {
+    const isWon = () => {
+        const board = gameBoard.getBoard()
+        const winningCombinations = gameBoard.getWinningCombinations()
         let i = 0
         while (i < winningCombinations.length) {
             let equal = true
